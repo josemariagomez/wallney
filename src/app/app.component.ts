6d01c8201841ecd9db6164b6759317bddb387579
JM <josegm1999@gmail.com>
2020-05-10 19:38:32 +0200

    fix: show status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent {
   
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleBlackOpaque();
       this.splashScreen.hide();
     });
   }
