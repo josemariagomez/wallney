4976b0dc9c54c8dc8ae4bd9c5c95b06c22e2d555
JM <josegm1999@gmail.com>
2020-06-09 22:02:54 +0200

    get home data

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-tab1',
@@ -8,9 +9,36 @@ import { Router } from '@angular/router';
 })
 export class Tab1Page {
 
-  constructor(private router: Router) {}
+  data;
+  routerSubscription;
 
-  ngOnInit(){
+  constructor(private router: Router, public api: ApiService) {}
+
+  async ngOnInit(){
+    this.routerWatch()
+    this.getData();
+  }
+  routerWatch() {
+    this.routerSubscription = this.router.events.subscribe(
+      (event: NavigationEnd) => {
+        if(event instanceof NavigationEnd) {
+            this.data = '';
+            this.getData();
+        }
+      }
+    );
+  }
+
+  ionPageWillLeave() {
+      this.routerSubscription.unsubscribe();
+  }
+  
+  async getData() {
+    this.api.getMainData().then((data) => {
+      console.log(data)
+      this.data = JSON.parse(JSON.stringify(data)).body;
+      console.log(this.data);
+    });
   }
   goGastos(){
     this.router.navigateByUrl('/gastos')
